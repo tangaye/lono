62166148632472d279ee26feeea0348d6eb14869
Nathan Tangaye Siafa <nathan@kwagei.com>
2019-04-28 22:51:39 +0000

    affixed header and footer at top and bottom, color recipient message blue

--- app.js
+++ app.js
@@ -1,24 +1,58 @@
+const messageInputEl = document.getElementById('message');
+const messengerEl = document.getElementById('messenger');
+const conversationEl = document.querySelector('.conversation');
+
 // A. a place to store chat messages
 const messages = [];
 
 // B. way to add new chat messages
-const addMessage = (sender, message) => {
+const addMessage = (message) => {
+
+    const messenger = messengerEl.value === 'sender' ? 'Blama Doe' : 'Konah Doe';
+
+    const newMessage = {
+        messenger: messenger, 
+        message: message,
+        date: new Date()
+    };
 
     // C. assign a sender to a message 
-    messages.push({sender: sender, message: message});
-    
+    messages.push(newMessage);
+
+    displayMessage(newMessage);
+
 }
 
 // D. Way to display the sender and the chat message
-const displayMessage = () => {
+const displayMessage = (message) => {
 
-    const message = `
-        <div class="first-sender__message">
-            <p class="message">${messages[0].message}</p>
-            <p class="sender">${messages[0].sender}</p>
-        </div>
+    const date = message.date;
+    const dateString = `${date.toDateString()} • ${date.toLocaleTimeString()}`;
+
+    const messageBlock = `
+        <li class="${messengerEl.value}">
+            <div class="messages">
+                <p class="message">${message.message}</p>
+                <span class="sender">${message.messenger}</span>
+                <time>${dateString}</time>
+            </div>
+        </li>
     `;
 
     // F. way to display chat message above chat box field
-    document.getElementById('messages').insertAdjacentHTML('beforeend', message);
+    conversationEl.insertAdjacentHTML('beforeend', messageBlock);
 }
+
+
+messageInputEl.addEventListener('keydown', (event) => {
+    
+    const message = messageInputEl.value;
+
+    if (event.code !== 'Enter') return;
+    if (message.length === 0) return;
+    if (messengerEl.value === '') return;
+
+    addMessage(message);
+
+});
+
