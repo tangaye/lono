112a7c2900bfce1e53e33bad460425113a2d0aea
Nathan Tangaye Siafa <nathan@kwagei.com>
2019-05-29 19:36:29 +0000

    refactor code

--- app.js
+++ app.js
@@ -1,79 +1,139 @@
-const messageInputEl = document.getElementById('message');
-const messengerEl = document.getElementById('messenger');
-const conversationEl = document.querySelector('.conversation');
-const container = document.querySelector('#container');
-const searchBox = document.querySelector('.search-box');
+const pageEl = {
+    messageInputEl: document.querySelector('#message'),
+    messengerEl: document.querySelector('#messenger'),
+    conversationEl: document.querySelector('.conversation'),
+    container: document.querySelector('#container'),
+    searchBox: document.querySelector('.search-box')
+};
 
-// A. a place to store chat messages
-const messages = [];
+const chatModel = {
 
-// B. way to add new chat messages
-const addMessage = (message) => {
+    messages: [],
 
-    const messenger = messengerEl.value === 'sender' ? 'Blama Doe' : 'Konah Doe';
+    messenger: '',
 
-    const newMessage = {
-        messenger: messenger,
-        message: message,
-        date: new Date()
-    };
+    addMessage(message) {
+        this.messages.push(message);
+    },
 
-    // C. assign a sender to a message 
-    messages.push(newMessage);
+    setMessenger(messenger) {
+        this.messenger = messenger;
+    },
 
-    displayMessage(newMessage);
-
-    updateScroll();
-
+    getMessenger() {
+        return this.messenger;
+    }
 }
 
-// D. Way to display the sender and the chat message
-const displayMessage = (message) => {
+const chatController = {
 
-    const date = message.date;
-    const dateString = `${date.toDateString()} • ${date.toLocaleTimeString()}`;
+    sendMessage(message) {
 
-    const messageBlock = `
-        <li class="${messengerEl.value} message-block">
-            <div class="messages">
-                <p class="message">${message.message}</p>
-                <span class="sender">${message.messenger}</span>
-                <time>${dateString}</time>
-            </div>
-        </li>
-    `;
+        const newMessage = {
+            messenger: chatModel.getMessenger(),
+            message: message,
+            date: new Date()
+        };
 
-    // F. way to display chat message above chat box field
-    conversationEl.insertAdjacentHTML('beforeend', messageBlock);
+        // add message
+        chatModel.addMessage(newMessage);
+
+        // display message
+        this.displayMessage(newMessage);
+
+        // keep page scroll at the bottom
+        updateScroll();
+
+    },
+
+    displayMessage(message) {
+        chatView.displayMessage(message);
+    },
+
+    setChatMessenger(messenger) {
+
+        chatModel.setMessenger(messenger);
+    },
+
+    search(needle) {
+
+        // Get an iterable NodeList of all chat messages(message-block)
+        const messageBlocks = document.querySelectorAll('.message-block');
+
+        // loop through messageBlocks
+        messageBlocks.forEach(haystack => {
+
+            // A user message sits in a <p> which sits in a <div>
+            let haystackText = haystack.firstElementChild.firstElementChild.textContent.toUpperCase();
+
+            // if needle found in haystack display message otherwise hide it
+            haystackText.indexOf(needle) !== -1 ? haystack.style.display = 'flex' : haystack.style.display = 'none';
+
+        });
+
+    }
 }
+
+const chatView = {
+
+    displayMessage(message) {
+
+        const date = message.date;
+        const dateString = `${date.toDateString()} • ${date.toLocaleTimeString()}`;
+
+        const messageBlock = `
+            <li class="${pageEl.messengerEl.value} message-block">
+                <div class="messages">
+                    <p class="message">${message.message}</p>
+                    <span class="sender">${message.messenger}</span>
+                    <time>${dateString}</time>
+                </div>
+            </li>
+        `;
+
+        pageEl.conversationEl.insertAdjacentHTML('beforeend', messageBlock);
+
+    },
+
+    getChatMessenger() {
+
+        // If messengerEl value is 'sender'  messenger is 'Blama Doe' otherwise 'Konah Doe'
+        return pageEl.messengerEl.value === 'sender' ? 'Blama Doe' : 'Konah Doe';
+
+    },
+
+    setupEventListeners() {
+
+        // Event listener for search box
+        pageEl.searchBox.addEventListener('keyup', event => {
+
+            let searchChar = event.target.value.toUpperCase();
+
+            chatController.search(searchChar);
+
+        });
+
+        // Event listener for message input
+        pageEl.messageInputEl.addEventListener('keydown', event => {
+
+            const message = event.target.value;
+            let messenger = this.getChatMessenger();
+
+            if (event.code !== 'Enter') return; // Do nothing when user doesn't press enter to send message
+            if (message.length === 0) return; // Do nothing when message input is empty
+            if (messenger === '') return; // Do nothing when messenger is not selected
+
+            // otherwise set messenger and  send message
+
+            chatController.setChatMessenger(messenger);
+            chatController.sendMessage(message);
+
+        });
+
+    }
+}
+
+chatView.setupEventListeners();
+
 
 const updateScroll = () => container.scrollTop = container.scrollHeight;
-
-messageInputEl.addEventListener('keydown', event => {
-
-    const message = messageInputEl.value;
-
-    if (event.code !== 'Enter') return;
-    if (message.length === 0) return;
-    if (messengerEl.value === '') return;
-
-    addMessage(message);
-
-});
-
-
-searchBox.addEventListener('keyup', event => {
-
-    let searchChar = event.target.value.toUpperCase();
-
-    const messageBlocks = document.querySelectorAll('.message-block');
-
-    messageBlocks.forEach(message => {
-
-        let messageText = message.firstElementChild.firstElementChild.textContent.toUpperCase();
-
-        messageText.indexOf(searchChar) !== -1 ? message.style.display = 'flex' : message.style.display = 'none';
-
-    });
-
-});
