9c62206d5485215788baf7eba2f38c3876d16beb
mrhumble <carlos@kwagei.com>
2021-05-28 13:14:40 +0000

    Remove swagger header and favicons

--- public/js/swag.js
+++ public/js/swag.js
@@ -1,12 +1,20 @@
+const HideInfoUrlPartsPlugin = () => {
+  return {
+    wrapComponents: {
+      InfoUrl: () => () => null
+    }
+  }
+}
 window.onload = function () {
     // Begin Swagger UI call region
     const ui = SwaggerUIBundle({
       url: "/lono.json",
       dom_id: "#swagger-ui",
-      deepLinking: true,
-      presets: [SwaggerUIBundle.presets.apis, SwaggerUIStandalonePreset],
-      plugins: [SwaggerUIBundle.plugins.DownloadUrl],
-      layout: "StandaloneLayout",
+      deepLinking: false,
+      presets: [SwaggerUIBundle.presets.apis,
+        ],
+      plugins: [HideInfoUrlPartsPlugin],
+      // layout: "StandaloneLayout",
     });
     // End Swagger UI call region
 
