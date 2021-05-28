57b18d4e5341eab84ab117496ff19cfee6d0286b
Nathan Tangaye Siafa <nathan@kwagei.com>
2021-05-28 11:18:55 +0000

    Development

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ const Client = require("./models/Client");
 const Message = require("./models/Message");
 const Sender = require("./models/Sender");
 
+const apiRoutes = require("./routes/api.docs.route");
 const messagesRoutes = require("./routes/messages");
 const clientRoutes = require("./routes/clients");
 
@@ -20,8 +21,10 @@ app.use(helmet());
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
 
 // routes setup
+app.use(apiRoutes);
 app.use(messagesRoutes);
 app.use(clientRoutes);
 
---
+++ public/js/swag.js
@@ -0,0 +1,14 @@
+window.onload = function () {
+    // Begin Swagger UI call region
+    const ui = SwaggerUIBundle({
+      url: "/lono.json",
+      dom_id: "#swagger-ui",
+      deepLinking: true,
+      presets: [SwaggerUIBundle.presets.apis, SwaggerUIStandalonePreset],
+      plugins: [SwaggerUIBundle.plugins.DownloadUrl],
+      layout: "StandaloneLayout",
+    });
+    // End Swagger UI call region
+
+    window.ui = ui;
+  };
