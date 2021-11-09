7f7e124aaa146e78988613635cb4b3fb73c74356
tangaye <siafanathan@gmail.com>
2021-11-09 12:30:27 +0000

    customized job queries for Cititrust

--- services/JobsQueries.js
+++ services/JobsQueries.js
@@ -9,7 +9,8 @@ const logger = require("../logger")
 exports.findPendingFailedMessages = async () => {
     try {
 
-        let messages = await database.query(`SELECT messages.id, senders.name as "sender.name", senders.id as "sender.id", recipient as to, message as body FROM messages INNER JOIN senders ON messages.sender_id = senders.id WHERE (status = 'pending' OR status = 'failed') AND (CURRENT_DATE >= messages.created_at::date) AND (CURRENT_TIME - INTERVAL '30 minutes' > messages.created_at::time) AND (retries IS NULL OR retries <> 1)LIMIT 10`, {
+        let messages = await database.query(`SELECT messages.id, senders.name as "sender.name", senders.id as "sender.id", recipient as to, message as body FROM messages INNER JOIN senders ON messages.sender_id = senders.id WHERE (status = 'pending' OR status = 'failed') AND (CURRENT_DATE >= messages.created_at::date) AND (retries IS NULL OR retries <> 1)
+        AND (sender_id = '247a3182-743c-4a55-abff-cbe3e380b8cd') LIMIT 10`, {
             nest: true,
             type: QueryTypes.SELECT
         })
