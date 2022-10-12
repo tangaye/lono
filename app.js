bdadb7eb191ff3a5f336dab739a9b88a951e3fed
tangaye <siafanathan@gmail.com>
2022-10-12 16:23:18 +0000

    updated express json limit

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ const groupRoutes = require("./routes/groups")
 
 // app.use(helmet())
 app.use(cors())
-app.use(express.json())
+app.use(express.json({limit: '200mb'}))
 app.use(express.urlencoded({extended: true, limit: '200mb'}))
 app.use(express.static(path.join(__dirname, "public")))
 
