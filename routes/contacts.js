c98002195406460609f8409af769266cf0753e9d
tangaye <siafanathan@gmail.com>
2023-10-06 04:07:12 +0000

    changed contacts post url

--- routes/contacts.js
+++ routes/contacts.js
@@ -3,11 +3,11 @@ const ContactsController = require("../controllers/ContactsController");
 const {authenticate} = require("../middlewares")
 const {validateAll, validateCreate, validateStore} = require("../validators/contacts")
 
-router.post('/contacts/create', [authenticate, validateCreate], ContactsController.create)
+// router.post('/contacts/create', [authenticate, validateCreate], ContactsController.create)
 
 router.route('/contacts')
 	.get([authenticate, validateAll], ContactsController.all)
-	.post([authenticate, validateStore], ContactsController.store)
+	.post([authenticate, validateCreate], ContactsController.create)
 
 router.get('/contacts/:id', authenticate, ContactsController.get)
 router.delete('/contacts/:id', authenticate, ContactsController.remove)
